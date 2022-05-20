c8300721d5e0eaa18834f1668c1438b739c5293c
ravensoni <raven3dc@gmail.com>
2022-05-21 00:12:40 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require('express')
+const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 
-app.listen(8000, () => {
-    console.log('server is running!');
+const mockUserData = [
+	{name: 'Elon'},
+	{name: 'Rahul'}
+];
+
+app.get('/users', (req, res) => {
+	res.json({
+		success: true,
+		message: "successfuly got users. Nice.",
+		users: mockUserData
+	})
 })
 
+app.listen(8000, () => {
+	console.log('hey raven, server is listening.');
+})
